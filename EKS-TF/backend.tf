a8097da88293c2bce0b570d068a13f4f90f95236
Pratap Padhy <pratappadhy2005@gmail.com>
2026-02-14 22:52:16 +1100

    -updated few config things

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = "dev-pratap-tf-bucket"
     region       = "ap-southeast-2"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     use_lockfile = true
